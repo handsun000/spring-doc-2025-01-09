d902fae972bee4e06a900c07c6105c0077aa1d78
jhs512 <jangka512@gmail.com>
2025-01-09 15:16:51 +0900

    work

--- backend/src/main/java/com/ll/spring_doc_2025_01_09/SpringDoc20250109Application.java
+++ backend/src/main/java/com/ll/spring_doc_2025_01_09/SpringDoc20250109Application.java
@@ -2,12 +2,14 @@ package com.ll.spring_doc_2025_01_09;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringDoc20250109Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDoc20250109Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDoc20250109Application.class, args);
+    }
 
 }
