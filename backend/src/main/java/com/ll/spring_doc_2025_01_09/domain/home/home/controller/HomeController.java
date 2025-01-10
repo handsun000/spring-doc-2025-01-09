07584c8882fc345204ae7d5fe8037b882f2a4145
jhs512 <jangka512@gmail.com>
2025-01-10 09:23:29 +0900

    work

--- backend/src/main/java/com/ll/spring_doc_2025_01_09/domain/home/home/controller/HomeController.java
+++ backend/src/main/java/com/ll/spring_doc_2025_01_09/domain/home/home/controller/HomeController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Tag(name = "HomeController", description = "홈 컨트롤러")
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = "text/html;charset=utf-8")
     @ResponseBody
     @Operation(summary = "메인 페이지")
     public String main() {
-        return "API 서버 입니다.";
+        return "<h1>API 서버 입니다.</h1>";
     }
 }
