e8d6acd82f9e14902282e6cc859b9d1a7e90372c
jhs512 <jangka512@gmail.com>
2025-01-10 09:41:13 +0900

    work

--- backend/src/main/java/com/ll/spring_doc_2025_01_09/domain/post/post/controller/ApiV1PostController.java
+++ backend/src/main/java/com/ll/spring_doc_2025_01_09/domain/post/post/controller/ApiV1PostController.java
@@ -112,6 +112,7 @@ public class ApiV1PostController {
 
     @PostMapping
     @Transactional
+    @Operation(summary = "글 작성")
     public RsData<PostWithContentDto> write(
             @RequestBody @Valid PostWriteReqBody reqBody
     ) {
