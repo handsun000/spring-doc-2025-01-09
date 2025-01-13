c15432101931fac0fad2229a1346b0d3c43ef162
son <handsun000@gmail.com>
2025-01-13 11:24:59 +0900

    work

--- frontend/app/post/list/page.tsx
+++ frontend/app/post/list/page.tsx
@@ -1,26 +1,23 @@
 import type { components } from "@/src/lib/backend/apiV1/schema";
 
 type PostDto = components["schemas"]["PostDto"];
-
-type PostItemPageDto = {
-  currentPageNumber: number;
-  pageSize: number;
-  totalPages: number;
-  totalItems: number;
-  items: PostDto[];
-};
+type PageDtoPostDto = components["schemas"]["PageDtoPostDto"];
 
 export default async function Page() {
   const response = await fetch("http://localhost:8080/api/v1/posts");
-  const body: PostItemPageDto = await response.json();
+  const body: PageDtoPostDto = await response.json();
   return (
     <div>
       <div>currentPageNumber: {body.currentPageNumber}</div>
+
       <div>pageSize: {body.pageSize}</div>
+
       <div>totalPages: {body.totalPages}</div>
+
       <div>totalItems: {body.totalItems}</div>
+
       <ul>
-        {body.items.map((item: PostDto) => (
+        {body.items?.map((item: PostDto) => (
           <li key={item.id} className="border-[2px] border-[red] my-3">
             <div>id : {item.id}</div>
             <div>createDate : {item.createDate}</div>
